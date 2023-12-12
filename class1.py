2c165947ae4dcabc6b191e673fa2c8871382b928
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-12-12 18:44:10 +0530

    mobile exam1

---
+++ class1.py
@@ -0,0 +1,11 @@
+class vivo:
+    def __init__(self):
+        print("elcome to vivo mobiles")
+
+    def example(self):
+        print("x50","1234","256gb")
+#object
+mob1=vivo()
+mob1.example()
+#function calling
+#acess
